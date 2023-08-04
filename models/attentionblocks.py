be644d11843bd53ca40e596840d2a584ce7b38e5
Agus Castillo <agu13xd@hotmail.com>
2023-08-03 21:46:53 -0600

    Gabor

--- models/attentionblocks.py
+++ models/attentionblocks.py
@@ -3,6 +3,48 @@ import torch.nn as nn
 import pdb
 
 
+class GAMReal_Module(nn.Module):
+    """
+    Gabor attention module
+    """
+    def __init__(self, no_g):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.align = nn.Softmax(dim=-1)
+        self.no_g = no_g
+
+    def forward(self, x):
+        """
+        Parameters:
+        ----------
+            inputs :
+                x : input feature maps( B X C X G X H X W)
+            returns :
+                out : attention value + input feature
+        """
+        m_batchsize, C, height, width = x.size()
+        C = C // self.no_g
+        proj_query = x.view(m_batchsize, self.no_g, -1)
+        proj_key = x.view(m_batchsize, self.no_g, -1).permute(0, 2, 1)
+
+        energy = torch.bmm(proj_query, proj_key)
+        energy = torch.max(energy, -1, keepdim=True)[0].expand_as(energy) - energy
+        attention = self.align(energy)
+        proj_value = x.view(m_batchsize, self.no_g, -1)
+
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C * self.no_g, height, width)
+
+        out = self.gamma * out + x
+        return out
+
+if __name__ == '__main__':
+     
+     tensor = torch.randn((8, 3, 512, 512))
+     block = GAMReal_Module(3)
+     pred = block(tensor)
+     print(pred.size())
+
 class GAB(nn.Module):
     def __init__(self, in_planes):
         super(GAB, self).__init__()
